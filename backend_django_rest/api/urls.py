41c623fc2273c3590d06448f6e3b75e199c385a1
Smolllll1 <Smolllll192@gmail.com>
2023-05-12 18:57:21 +0300

    add pagination1

--- backend_django_rest/api/urls.py
+++ backend_django_rest/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import PeopleViewSet, MoviesViewSet, pop_people
+from .views import PeopleViewSet, MoviesViewSet
 from rest_framework import routers
 
 
@@ -11,14 +11,14 @@ router.register(r'pop_people', PeopleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)), #http://127.0.0.1:8000/
-    path('test/', views.search_tast, name='test'),
+    # path('test/', views.search_tast, name='test'),
     path('add_movies/', views.pop_movies, name='movies'),
     path('add_movies/<str:pk>', views.pop_movies, name='movies_pk'),    
     path('add_people/', views.pop_people, name='people'),
     path('add_people/<str:pk>', views.pop_people, name='people_pk'),
     path('registration/', views.register, name='registration'),
     path('cooky_login/', include('rest_framework.urls')),
-    path('login/', views.loginView, name='login'),
-    path('logout/', views.logout_view, name='logout'), 
-    path('search/', views.search, name='search'),   
+    # path('login/', views.user, name='login'),
+    # path('logout/', views.logout_view, name='logout'), 
+    # path('search/', views.search, name='search'),   
 ]
--- backend_django_rest/backend_django_rest/settings.py
+++ backend_django_rest/backend_django_rest/settings.py
@@ -16,12 +16,16 @@ from .key import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+from datetime import timedelta
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+# CSRF_COOKIE_SECURE = False
+# CSRF_COOKIE_HTTPONLY = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,9 +43,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "api.apps.ApiConfig",
+    'api.apps.ApiConfig',
     'rest_framework',
-    "corsheaders",
+    # 'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -131,15 +137,65 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': 
+    [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    # (
+    # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25,
 
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
-     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
+# SIMPLE_JWT = {
+#     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+#     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
+#     "ROTATE_REFRESH_TOKENS": True,
+#     "BLACKLIST_AFTER_ROTATION": True,
+#     "UPDATE_LAST_LOGIN": False,
+
+#     "ALGORITHM": "HS256",
+#     "VERIFYING_KEY": "",
+#     "AUDIENCE": None,
+#     "ISSUER": None,
+#     "JSON_ENCODER": None,
+#     "JWK_URL": None,
+#     "LEEWAY": 0,
+
+#     "AUTH_HEADER_TYPES": ("Bearer",),
+#     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+#     "USER_ID_FIELD": "id",
+#     "USER_ID_CLAIM": "user_id",
+#     "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+#     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+#     "TOKEN_TYPE_CLAIM": "token_type",
+#     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+#     "JTI_CLAIM": "jti",
+
+#     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+#     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+#     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+#     "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+#     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+#     "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+#     "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+#     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+#     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True
