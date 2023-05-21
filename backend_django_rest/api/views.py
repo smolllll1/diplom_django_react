1653fd00b344db998c3ef74b6aa95b67605f05a9
Smolllll1 <Smolllll192@gmail.com>
2023-05-21 08:05:30 +0300

    fix response total_page

--- backend_django_rest/api/views.py
+++ backend_django_rest/api/views.py
@@ -24,6 +24,11 @@ class DataPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data['total_pages'] = self.page.paginator.num_pages
+        return response
+
 class SearchPagination(PageNumberPagination):
     page_size = 10000
     page_size_query_param = 'page_size'
